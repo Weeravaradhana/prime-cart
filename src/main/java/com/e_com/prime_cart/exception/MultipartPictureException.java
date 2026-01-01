fd5060e1a5eb1b2cd064080021941dacd1528593
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 12:52:13 +0530

    feat(exception): add MultipartPictureException for multipart file handling errors

---
+++ src/main/java/com/e_com/prime_cart/exception/MultipartPictureException.java
@@ -0,0 +1,7 @@
+package com.e_com.prime_cart.exception;
+
+public class MultipartPictureException extends RuntimeException {
+  public MultipartPictureException(String message) {
+    super(message);
+  }
+}
