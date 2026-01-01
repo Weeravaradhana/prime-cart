c0af659defab84c8b4bc5163bfde722ce447f86b
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 14:01:38 +0530

    Add AbstractAuditingEntity with JPA auditing fields createDate and lastModifiedDate

---
+++ src/main/java/com/e_com/prime_cart/common/AbstractAuditingEntity.java
@@ -0,0 +1,31 @@
+package com.e_com.prime_cart.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class AbstractAuditingEntity<T> implements Serializable {
+
+    public abstract T getId();
+
+    @CreatedDate
+    @Column(updatable = false,name = "create_date")
+    private Instant createDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate;
+
+}
