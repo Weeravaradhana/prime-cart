10e609a95160f67a1e5e4e2d79ef4b66b64c7e8d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 21:04:36 +0530

    Add RestAuthority DTO with utility method to convert Authority set to String set

---
+++ src/main/java/com/e_com/prime_cart/builder/dto/authority/RestAuthority.java
@@ -0,0 +1,17 @@
+package com.e_com.prime_cart.builder.dto.authority;
+import com.e_com.prime_cart.builder.Authority;
+import org.jilt.Builder;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Builder
+public record RestAuthority(String name) {
+
+  public static Set<String> fromSet(Set<Authority> authorities) {
+    return authorities.stream()
+      .map(authority -> authority.getName().name())
+      .collect(Collectors.toSet());
+  }
+
+}
