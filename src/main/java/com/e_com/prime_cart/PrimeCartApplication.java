4ea1e9473e274f01db761b5b4b1f34de73c13c1e
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 13:04:23 +0530

    Initialize PrimeCart Spring Boot application.

---
+++ src/main/java/com/e_com/prime_cart/PrimeCartApplication.java
@@ -0,0 +1,13 @@
+package com.e_com.prime_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeCartApplication.class, args);
+	}
+
+}
