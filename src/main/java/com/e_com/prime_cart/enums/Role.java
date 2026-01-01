72439ec432f0122d876b69c845dec0765ecf0854
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 21:17:05 +0530

    Add Role enum with ADMIN, USER, ANONYMOUS, UNKNOWN; includes mapping from string with ROLE_ prefix and from() method for safe conversion

--- src/main/java/com/e_com/prime_cart/enums/Role.java
+++ src/main/java/com/e_com/prime_cart/enums/Role.java
@@ -7,7 +7,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public enum Role {
+public enum Role{
     ADMIN,
     USER,
     ANONYMOUS,
