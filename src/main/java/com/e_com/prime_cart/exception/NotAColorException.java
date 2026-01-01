cbd95850a7638883e28334765424d22c27cafe6e
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 12:52:50 +0530

    feat(exception): add NotAColorException for invalid color values

---
+++ src/main/java/com/e_com/prime_cart/exception/NotAColorException.java
@@ -0,0 +1,13 @@
+package com.e_com.prime_cart.exception;
+
+public class NotAColorException extends AssertionException {
+
+    public NotAColorException(String field, String message) {
+        super(field, message);
+    }
+
+    @Override
+    public AssertionErrorType type() {
+        return AssertionErrorType.NOT_A_COLOR;
+    }
+}
