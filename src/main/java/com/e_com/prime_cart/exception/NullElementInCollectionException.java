ab5f8ecc7212134a19128612b1c3c6bf21901aec
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 12:55:49 +0530

    feat(assertion): add NullElementInCollectionException for collections containing nulls

---
+++ src/main/java/com/e_com/prime_cart/exception/NullElementInCollectionException.java
@@ -0,0 +1,17 @@
+package com.e_com.prime_cart.exception;
+
+public class NullElementInCollectionException extends AssertionException {
+
+  public NullElementInCollectionException(String field) {
+    super(field, message(field));
+  }
+
+  private static String message(String field) {
+    return new StringBuilder().append("The field \"").append(field).append("\" contains a null element").toString();
+  }
+
+  @Override
+  public AssertionErrorType type() {
+    return AssertionErrorType.NULL_ELEMENT_IN_COLLECTION;
+  }
+}
