b90476ac703c0b19e8b246edd83474ec52142b79
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-01 13:28:41 +0530

    Add CORS configuration method to SecurityConfiguration for Angular frontend

--- src/main/java/com/e_com/prime_cart/config/SecurityConfiguration.java
+++ src/main/java/com/e_com/prime_cart/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.e_com.prime_cart.security;
+package com.e_com.prime_cart.config;
 
+import com.e_com.prime_cart.security.KeycloakAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,5 +34,18 @@ public class SecurityConfiguration {
                 );
         return http.build();
 
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // Angular app
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
